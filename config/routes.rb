b7cf5447d84e7dd5323e63c91b77651d04d6186e
Jonathan Dinu <jdinu@berkeley.edu>
2013-02-17 06:02:16 -0800

    repo is finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Clams::Application.routes.draw do
-
+  # Access API description at /api_taster
   mount ApiTaster::Engine => "/api_taster" if Rails.env.development?
   
   resources :users
@@ -69,27 +69,178 @@ end
 
 if Rails.env.development?
   ApiTaster.routes do
-    desc 'Get a __list__ of users'
-    get '/users'
+      # users
+      desc 'Get a __list__ of users'
+      get '/users'
 
-    post '/users', {
-      :user => {
-        :name => 'Fred'
+      desc "Create a new user"
+      post '/users', {
+        :user => {
+          :name => 'Susie',
+          :email => "susie@aol.com"
+        }
       }
-    }
 
-    get '/users/:id', {
-      :id => 1
-    }
-
-    put '/users/:id', {
-      :id => 1, :user => {
-        :name => 'Awesome'
+      desc "Get an individual user"
+      get '/users/:id', {
+        :id => 1
       }
-    }
 
-    delete '/users/:id', {
-      :id => 1
-    }
+      desc "Update an individual user"
+      put '/users/:id', {
+        :id => 1, :user => {
+          :name => "Jane",
+          :email => 'susieq@gmail.com'
+        }
+      }
+
+      desc "Delete a user"
+      delete '/users/:id', {
+        :id => 1
+      }
+
+      # comments
+      desc 'Get a __list__ of comments'
+      get '/comments'
+
+      desc "Create a new comment"
+      post '/comments', {
+        :comment => {
+          :content => 'Some Text',
+        }
+      }
+
+      desc "Get an individual comment"
+      get '/comments/:id', {
+        :id => 1
+      }
+
+      desc "Update an individual comment"
+      put '/comments/:id', {
+        :id => 1, :comment => {
+          :content => 'New Text'
+        }
+      }
+
+      desc "Delete a comment"
+      delete '/comments/:id', {
+        :id => 1
+      }
+
+      # groups
+      desc 'Get a __list__ of groups'
+      get '/groups'
+
+      desc "Create a new group"
+      post '/groups', {
+        :group => {
+          :name => 'Developers of the World',
+          :motto => "Hack the Planet"
+        }
+      }
+
+      desc "Get an individual group"
+      get '/groups/:id', {
+        :id => 1
+      }
+
+      desc "Update an individual group"
+      put '/groups/:id', {
+        :id => 1, :group => {
+          :name => "Atari Appreciation",
+          :motto => 'All your base are belong to us'
+        }
+      }
+
+      desc "Delete a group"
+      delete '/groups/:id', {
+        :id => 1
+      }
+
+      # posts
+      desc 'Get a __list__ of posts'
+      get '/posts'
+
+      desc "Create a new post"
+      post '/posts', {
+        :post => {
+          :title => 'Super Sweet Post',
+          :content => "I am the best writer"
+        }
+      }
+
+      desc "Get an individual post"
+      get '/posts/:id', {
+        :id => 1
+      }
+
+      desc "Update an individual post"
+      put '/posts/:id', {
+        :id => 1, :post => {
+          :title => "Humble beginnings",
+          :content => 'Writing is hard'
+        }
+      }
+
+      desc "Delete a post"
+      delete '/posts/:id', {
+        :id => 1
+      }
+
+      # statuses
+      desc 'Get a __list__ of statuses'
+      get '/statuses'
+
+      desc "Create a new status"
+      post '/statuses', {
+        :status => {
+          :content => 'Sleepy....ZZzZzZ',
+        }
+      }
+
+      desc "Get an individual status"
+      get '/statuses/:id', {
+        :id => 1
+      }
+
+      desc "Update an individual status"
+      put '/statuses/:id', {
+        :id => 1, :status => {
+          :content => 'Caffeine Rocks!'
+        }
+      }
+
+      desc "Delete a status"
+      delete '/statuses/:id', {
+        :id => 1
+      }
+
+      # subscriptions
+      desc 'Get a __list__ of subscriptions'
+      get '/subscriptions'
+
+      desc "Create a new subscription"
+      post '/subscriptions', {
+        :subscription => {
+          :renew_date => DateTime.now,
+        }
+      }
+
+      desc "Get an individual subscription"
+      get '/subscriptions/:id', {
+        :id => 1
+      }
+
+      desc "Update an individual subscription"
+      put '/subscriptions/:id', {
+        :id => 1, :subscription => {
+          :renew_date => DateTime.now
+        }
+      }
+
+      desc "Delete a subscription"
+      delete '/subscriptions/:id', {
+        :id => 1
+      }
   end
 end
